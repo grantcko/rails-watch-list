41d5b4c7df0dddad260f5ec9ee6451b82214c3a8
Grant Hall <grantclone@gmail.com>
2023-02-17 13:59:26 +0900

    changed root to lists page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # root to: 'lists#index'
+  get '/', to: 'lists#index'
   resources :lists, only: %i[index new create show] do
     resources :bookmarks, only: %i[new create]
   end
